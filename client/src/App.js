354d01470f597f183c80be4e8af077ae0b0b57cf
Larry Kagan <larry@superiocity.com>
2022-04-09 22:58:28 -0400

    Add client app

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import PostCreate from "./PostCreate";
+
+const App = () => {
+    return <div className="container">
+        <h1>Create Post</h1>
+        <PostCreate />
+    </div>;
+}
+
+export default App;
---
+++ client/src/PostCreate.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { useState } from "react";
+
+const PostCreate = () => {
+  const [title, setTitle] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:4000/posts', { title })
+      .then(res => {
+          console.log(res)
+          setTitle()
+      })
+      .catch(err => console.log(err));
+  }
+
+    return <div>
+        <form action="" onSubmit={onSubmit}>
+            <div className="form-group">
+                <label htmlFor="">Title</label>
+                <input
+                    value={ title }
+                    onChange={ (e) => setTitle(e.target.value) }
+                    type="text"
+                    className="form-control"
+                    id=""
+                    placeholder="Title"
+                />
+            </div>
+            <button className="btn btn-primary mt-3">Submit</button>
+        </form>
+    </div>;
+}
+
+export default PostCreate;
