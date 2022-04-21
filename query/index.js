30d72bfd85c69dd1c80ae88d3718f43a99644cc1
Larry Kagan <larry@superiocity.com>
2022-04-21 13:53:48 -0400

    Query service saves event data

--- query/index.js
+++ query/index.js
@@ -6,12 +6,27 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {
+const posts = {};
 
+app.get('/posts', (req, res) => {
+    res.send(posts);
 });
 
 app.post('/events', (req, res) => {
+    const {type, data} = req.body;
 
+    if (type === 'PostCreated') {
+        const {id, title} = data;
+        posts[id] = {id, title, comments: []};
+    }
+
+    if (type === 'CommentCreated') {
+        const {id, comment, postId} = data;
+        posts[postId].comments.push({id, comment});
+    }
+
+    console.log(posts);
+    res.send({});
 });
 
 app.listen(4002, () => {
