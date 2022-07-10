a11a9b01ae36208b8d7d711ab30fabb0e424f9f1
Larry Kagan <larry@superiocity.com>
2022-07-10 13:21:53 -0400

    Use query service for post and comment listing

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,25 +1,10 @@
-import React, { useEffect } from 'react';
-import axios from "axios";
-
-const CommentList = ({ postId }) => {
-    const [comments, setComments] = React.useState([]);
-
-    const getComments = () => {
-        axios.get(`http://localhost:4001/posts/${postId}/comments`)
-            .then(res => { setComments(res.data); })
-            .catch(err => { console.log(err); });
-    };
-
-    useEffect(() => getComments(), []);
-
-    const renderedComments = comments.map((comment) => {
-        return (
-            <li key={comment.id}>{comment.content}</li>
-        )
-    });
-
+const CommentList = ({ comments }) => {
     return (
-        <ul>{renderedComments}</ul>
+        <ul>
+            {
+                comments.map(comment => <li key={comment.id}>{comment.content}</li>)
+            }
+        </ul>
     );
 };
 
